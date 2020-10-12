a47f2427c6ac2f62414cd660a7d39824c2e0a9f4
Harsh Agrawal <agrawalharsh90@gmail.com>
2020-10-12 16:04:01 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <div>
             <h1>Current Time!</h1>
             <h4>In this app we render element in DOM everytime</h4>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            <h2>It is {new Date().toLocaleString()}.</h2>
         </div>
     );
 }
