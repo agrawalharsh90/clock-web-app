bc5ba1f8d809d3c8b3265454011214b8ad394668
Harsh Agrawal <agrawalharsh90@gmail.com>
2020-11-02 09:12:56 +0530

    minor changes

--- src/components/FunctionalComponent.tsx
+++ src/components/FunctionalComponent.tsx
@@ -1,38 +1,27 @@
 import React, {useEffect, useState} from "react";
 
-interface FunctionalComponentState {
-    date: Date;
-    timerId: any;
-}
 
 const FunctionalComponent: React.FC = () => {
-    const [data, setData] = useState({
-        date: new Date(),
-    });
+    const [date, setDate] = useState(new Date());
 
     const tick: Function = () => {
-        setData({
-            date: new Date(),
-        });
+        setDate(new Date());
     }
 
     useEffect(() => {
-      let timerId=setInterval(
-            () => tick(),
-            1000
-        );
-      return()=>{
-          clearInterval(timerId);
-      }
+        let timerId = setInterval(() => tick(), 1000);
+        return () => {
+            clearInterval(timerId);
+        }
     });
 
     return (
         <div>
             <br/>
             <h5>#### In this app we use State in Functional Component</h5>
-            <h3>Date is {data.date.toLocaleDateString()}.</h3>
-            <h3>Time is {data.date.toLocaleTimeString()}.</h3>
-            <h3>DateTime is {data.date.toLocaleString()}.</h3>
+            <h3>Date is {date.toLocaleDateString()}.</h3>
+            <h3>Time is {date.toLocaleTimeString()}.</h3>
+            <h3>DateTime is {date.toLocaleString()}.</h3>
         </div>
     );
 }
